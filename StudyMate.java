5d6e4ed9bdc718f0ed956c2b64ad62440c08ad26
Justin Schaefer <schaef87@gmail.com>
2015-11-24 20:03:50 -0600

    Simple start.

---
+++ StudyMate.java
@@ -0,0 +1,12 @@
+// StudyMate V1.0
+// By: John VandeNoord & Justin Schaefer
+// Copyright 2015
+
+class StudyMate {
+   public static void main(String[] args){
+      System.out.println("Welcome to StudyMate!\n");
+      System.out.println("Created by: John VandeNoord and Justin Schaefer(USNR)\n");
+      System.out.println("Please make a selection:\n");
+      System.out.println("1. Take a Test\n" + "2. Make/Edit a Test");
+   } //End of Main
+} //End
--- main.java
+++
@@ -1,3 +0,0 @@
-/* StudyMate V1.0 */
-/* By: John VandeNoord & Justin Schaefer */
-/* Copyright 2015 */
